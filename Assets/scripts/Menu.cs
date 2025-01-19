f51bc77a093a2a9fe043e78dd8d0dcd399640f93
schoolchar <br295143@ucf.edu>
2025-01-19 13:50:02 -0500

    menu works

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void playButton()
+    {
+        SceneManager.LoadScene(1);
+    
+    }
+    public void stopButton() 
+    {
+        Application.Quit();
+
+    }
+}
+
