781211fc6abdd7328411de52430ecfea1b5bcfef
schoolchar <br295143@ucf.edu>
2025-01-18 20:46:36 -0500

    time done

--- Assets/scripts/fallingPiece.cs
+++ Assets/scripts/fallingPiece.cs
@@ -14,15 +14,11 @@ public class fallingPiece : MonoBehaviour
         Instantiate(losePic);
     }
 
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnMouseDown()
     {
-        
+        Destroy(gameObject);
     }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    private float time;
+    private float elapTime;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapTime += Time.deltaTime;
+        timerText.text = elapTime.ToString("F2");
+
+        if (elapTime >= 10)
+        {
+            Time.timeScale = 0;
+        }
+        if (time >= 10)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
