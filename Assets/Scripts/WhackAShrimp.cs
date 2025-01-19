3eac779f67b869a82a2b531a174ea81c5cef849b
jmola-gh <jacobmola789@gmail.com>
2025-01-19 01:25:20 -0500

    Finished Microgame

--- Assets/Scripts/WhackAShrimp.cs
+++ Assets/Scripts/WhackAShrimp.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class WhackAShrimp : MonoBehaviour
 {
-    bool isBroken = false;
+    public bool isWellBroken;
+    public bool amIFixed;
     [SerializeField] private Animator animator;
-
     GameObject shrimp;
+    public GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
         
 
@@ -23,13 +27,16 @@ public class WhackAShrimp : MonoBehaviour
 
     void OnMouseDown()
     {
-    if (isBroken == false) {
+    if (isWellBroken == false) {
         Debug.Log("Boioioioioing");
-            animator.SetTrigger("Appear");
+        animator.SetTrigger("Appear");
         }
-    else if (isBroken == true) {
-        Debug.Log("Shrimp machine broke");
-
+    else if (isWellBroken == true && amIFixed == false) {
+        amIFixed = true;
+        Debug.Log("I am fixed!");
+            gameManager.spotsFixed++;
+            Debug.Log(gameManager.spotsFixed + " holes are fixed");
+            gameManager.WinCondition();
         }  
 
     }
